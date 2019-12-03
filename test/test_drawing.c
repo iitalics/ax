7457ffdc0881198767edd770e2bcd9f7f7de67e8
Milo <iitalics@gmail.com>
2019-12-02 21:59:41 -0500

    change some tests to use sexps

--- test/test_drawing.c
+++ test/test_drawing.c
@@ -33,35 +33,28 @@ TEST(draw_1r)
     struct ax_state* s = ax_new_state();
     ax_read(s,
             "(set-dim 200 200)"
-            "(set-root (rect (fill \"ff0033\")"
-            "                (size 60 80)))");
+            "(set-root (rect (fill \"ff0033\") (size 60 80)))");
 
     const struct ax_drawbuf* d = ax_draw(s);
-
     CHECK_SZEQ(d->len, (size_t) 1);
     CHECK_IEQ(D(0).ty, AX_DRAW_RECT);
     CHECK_IEQ(D(0).r.fill, 0xff0033);
     CHECK_POSEQ(D(0).r.bounds.o, AX_POS(0.0, 0.0));
     CHECK_DIMEQ(D(0).r.bounds.s, AX_DIM(60.0, 80.0));
-
     ax_destroy_state(s);
 }
 
-/*TEST(draw_3r)
+TEST(draw_3r)
 {
     struct ax_state* s = ax_new_state();
-    ax_set_dimensions(s, AX_DIM(200, 200));
-    ax_set_root(
-        s,
-        &(struct ax_desc)
-        CONT(AX_JUSTIFY_BETWEEN, AX_JUSTIFY_CENTER,
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0xff0000, 60, 60)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x00ff00, 20, 20)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x0000ff, 60, 60)))
-        );
-
-    const struct ax_drawbuf* d = ax_draw(s);
-
+    ax_read(s,
+            "(set-dim 200 200)"
+            "(set-root"
+            " (container (children (rect (fill \"ff0000\") (size 60 60))"
+            "                      (rect (fill \"00ff00\") (size 20 20))"
+            "                      (rect (fill \"0000ff\") (size 60 60)))"
+            "            (main-justify between)"
+            "            (cross-justify center)))");
     //
     //   +--------------+
     //   |              |
@@ -73,25 +66,22 @@ TEST(draw_1r)
     //
     //   (see `./reference_draw_3r.html`)
     //
+    const struct ax_drawbuf* d = ax_draw(s);
     CHECK_SZEQ(d->len, (size_t) 3);
-
     CHECK_IEQ(D(0).ty, AX_DRAW_RECT);
     CHECK_IEQ_HEX(D(0).r.fill, 0xff0000);
     CHECK_POSEQ(D(0).r.bounds.o, AX_POS(0.0, 70.0));
     CHECK_DIMEQ(D(0).r.bounds.s, AX_DIM(60.0, 60.0));
-
     CHECK_IEQ(D(1).ty, AX_DRAW_RECT);
     CHECK_IEQ_HEX(D(1).r.fill, 0x00ff00);
     CHECK_POSEQ(D(1).r.bounds.o, AX_POS(90.0, 70.0));
     CHECK_DIMEQ(D(1).r.bounds.s, AX_DIM(20.0, 20.0));
-
     CHECK_IEQ(D(2).ty, AX_DRAW_RECT);
     CHECK_IEQ_HEX(D(2).r.fill, 0x0000ff);
     CHECK_POSEQ(D(2).r.bounds.o, AX_POS(140.0, 70.0));
     CHECK_DIMEQ(D(2).r.bounds.s, AX_DIM(60.0, 60.0));
-
     ax_destroy_state(s);
-} */
+}
 
 /*TEST(draw_text_1l)
 {
--- test/test_geometry.c
+++ test/test_geometry.c
@@ -5,23 +5,25 @@
 #define N(_id)  ax_node_by_id(&s->tree, _id)
 
 
-/*TEST(empty_root_node)
+TEST(empty_root_node)
 {
     struct ax_state* s = ax_new_state();
     CHECK_SZEQ(s->tree.count, (size_t) 1);
     CHECK_IEQ(N(0)->ty, AX_NODE_CONTAINER);
     ax_destroy_state(s);
-} */
+}
 
-/*TEST(build_tree)
+#define TWO_RECTS                               \
+    "(rect (fill \"ff0000\") (size 60 60))"     \
+    "(rect (fill \"0000ff\") (size 60 60))"
+
+TEST(build_tree)
 {
     struct ax_state* s = ax_new_state();
-    const struct ax_desc root = CONT(
-        AX_JUSTIFY_START, 0,
-        FLEX_CHILD(0, 1, 0, RECT(0xff0000, 60, 60)),
-        FLEX_CHILD(0, 1, 0, RECT(0x0000ff, 60, 60)));
-    ax_set_dimensions(s, AX_DIM(200, 200));
-    ax_set_root(s, &root);
+    ax_read(s,
+            "(set-dim 200 200)"
+            "(set-root"
+            " (container (children " TWO_RECTS ")))");
     CHECK_SZEQ(s->tree.count, (size_t) 3);
     CHECK_IEQ(N(0)->ty, AX_NODE_CONTAINER);
     CHECK_IEQ(N(1)->ty, AX_NODE_RECTANGLE);
@@ -29,40 +31,39 @@
     CHECK_IEQ_HEX(N(1)->r.fill, 0xff0000);
     CHECK_IEQ_HEX(N(2)->r.fill, 0x0000ff);
     ax_destroy_state(s);
-} */
+}
 
 /* Justification tests with two 60x60 rectangles, in a 200x200 window */
 
-#define JUSTIFY_TEST_2R(_mj, _xj, _x0, _y0, _x1, _y1) do {      \
-        struct ax_state* s = ax_new_state();                    \
-        const struct ax_desc root =                             \
-            CONT(_mj, _xj,                                      \
-                 FLEX_CHILD(0, 1, 0, RECT(0xff0000, 60, 60)),   \
-                 FLEX_CHILD(0, 1, 0, RECT(0x0000ff, 60, 60)));  \
-        ax_set_dimensions(s, AX_DIM(200, 200));                 \
-        ax_set_root(s, &root);                                  \
-        CHECK_POSEQ(N(1)->coord, AX_POS(_x0, _y0));             \
-        CHECK_POSEQ(N(2)->coord, AX_POS(_x1, _y1));             \
-        ax_destroy_state(s);                                    \
+#define JUSTIFY_TEST_2R(_mj, _xj, _x0, _y0, _x1, _y1) do {          \
+        struct ax_state* s = ax_new_state();                        \
+        ax_read(s,                                                  \
+                "(set-dim 200 200)"                                 \
+                "(set-root (container (children " TWO_RECTS ")"     \
+                "                     (main-justify " _mj ")"       \
+                "                     (cross-justify " _xj ")))");  \
+        CHECK_POSEQ(N(1)->coord, AX_POS(_x0, _y0));                 \
+        CHECK_POSEQ(N(2)->coord, AX_POS(_x1, _y1));                 \
+        ax_destroy_state(s);                                        \
     } while(0)
 
-/*TEST(main_justify_start_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_START, 0, 0, 0, 60, 0); } */
+TEST(main_justify_start_2r)
+{ JUSTIFY_TEST_2R("start", "start", 0, 0, 60, 0); }
 
-/*TEST(main_justify_end_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_END, 0, 80, 0, 140, 0); } */
+TEST(main_justify_end_2r)
+{ JUSTIFY_TEST_2R("end", "start", 80, 0, 140, 0); }
 
-/*TEST(main_justify_center_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_CENTER, 0, 40, 0, 100, 0); } */
+TEST(main_justify_center_2r)
+{ JUSTIFY_TEST_2R("center", "start", 40, 0, 100, 0); }
 
-/*TEST(main_justify_between_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_BETWEEN, 0, 0, 0, 140, 0); } */
+TEST(main_justify_between_2r)
+{ JUSTIFY_TEST_2R("between", "start", 0, 0, 140, 0); }
 
-/*TEST(main_justify_even_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_EVENLY, 0, 26.666, 0, 113.333, 0); } */
+TEST(main_justify_even_2r)
+{ JUSTIFY_TEST_2R("evenly", "start", 26.666, 0, 113.333, 0); }
 
-/*TEST(main_justify_around_2r)
-{ JUSTIFY_TEST_2R(AX_JUSTIFY_AROUND, 0, 20, 0, 120, 0); } */
+TEST(main_justify_around_2r)
+{ JUSTIFY_TEST_2R("around", "start", 20, 0, 120, 0); }
 
 #undef JUSTIFY_TEST_2R
 
@@ -92,19 +93,17 @@
 #undef TEXT_TEST
 
 
-/*TEST(spill_3r)
+TEST(spill_3r)
 {
     struct ax_state* s = ax_new_state();
-    ax_set_dimensions(s, AX_DIM(200.0, 200.0));
-    ax_set_root(
-        s,
-        &(struct ax_desc)
-        CONT(AX_JUSTIFY_START,
-             AX_JUSTIFY_START,
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0xff0000, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x00ff00, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x0000ff, 80.0, 80.0))));
-
+    ax_read(s,
+            "(set-dim 200 200)"
+            "(set-root"
+            " (container"
+            "  (children (rect (fill \"ff0000\") (size 80 80))"
+            "            (rect (fill \"00ff00\") (size 80 80))"
+            "            (rect (fill \"0000ff\") (size 80 80)))"
+            "  multi-line))");
     CHECK_SZEQ(s->tree.count, (size_t) 4);
     CHECK_SZEQ(N(0)->c.n_lines, (size_t) 2);
     CHECK_SZEQ(N(0)->c.line_count[0], (size_t) 2);
@@ -112,23 +111,19 @@
     CHECK_POSEQ(N(1)->coord, AX_POS(0.0, 0.0));
     CHECK_POSEQ(N(2)->coord, AX_POS(80.0, 0.0));
     CHECK_POSEQ(N(3)->coord, AX_POS(0.0, 80.0));
-} */
+}
 
-
-/*TEST(shrink_3r)
+TEST(shrink_3r)
 {
     struct ax_state* s = ax_new_state();
-    ax_set_dimensions(s, AX_DIM(200.0, 200.0));
-
-    struct ax_desc root =
-        CONT(AX_JUSTIFY_START,
-             AX_JUSTIFY_START,
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0xff0000, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x00ff00, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x0000ff, 80.0, 80.0)));
-    root.c.single_line = true;
-
-    ax_set_root(s, &root);
+    ax_read(s,
+            "(set-dim 200 200)"
+            "(set-root"
+            " (container"
+            "  (children (rect (fill \"ff0000\") (size 80 80))"
+            "            (rect (fill \"00ff00\") (size 80 80))"
+            "            (rect (fill \"0000ff\") (size 80 80)))"
+            "  single-line))");
     CHECK_SZEQ(s->tree.count, (size_t) 4);
     CHECK_SZEQ(N(0)->c.n_lines, (size_t) 1);
     CHECK_SZEQ(N(0)->c.line_count[0], (size_t) 3);
@@ -139,22 +134,19 @@
     CHECK_DIMEQ(N(2)->target, AX_DIM(66.66, 80.0));
     CHECK_DIMEQ(N(3)->target, AX_DIM(66.66, 80.0));
     ax_destroy_state(s);
-} */
+}
 
-/*TEST(shrink_3r_asym)
+TEST(shrink_3r_asym)
 {
     struct ax_state* s = ax_new_state();
-    ax_set_dimensions(s, AX_DIM(200.0, 200.0));
-
-    struct ax_desc root =
-        CONT(AX_JUSTIFY_START,
-             AX_JUSTIFY_START,
-             FLEX_CHILD(0, 0, AX_JUSTIFY_START, RECT(0xff0000, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x00ff00, 80.0, 80.0)),
-             FLEX_CHILD(0, 1, AX_JUSTIFY_START, RECT(0x0000ff, 80.0, 80.0)));
-    root.c.single_line = true;
-
-    ax_set_root(s, &root);
+    ax_read(s,
+            "(set-dim 200 200)"
+            "(set-root"
+            " (container"
+            "  (children (rect (fill \"ff0000\") (size 80 80) (shrink 0))"
+            "            (rect (fill \"00ff00\") (size 80 80))"
+            "            (rect (fill \"0000ff\") (size 80 80)))"
+            "  single-line))");
     CHECK_SZEQ(s->tree.count, (size_t) 4);
     CHECK_SZEQ(N(0)->c.n_lines, (size_t) 1);
     CHECK_SZEQ(N(0)->c.line_count[0], (size_t) 3);
@@ -165,4 +157,4 @@
     CHECK_DIMEQ(N(2)->target, AX_DIM(60.0, 80.0));
     CHECK_DIMEQ(N(3)->target, AX_DIM(60.0, 80.0));
     ax_destroy_state(s);
-} */
+}
