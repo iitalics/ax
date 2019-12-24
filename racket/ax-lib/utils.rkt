8301464c4b4a88e998893a16d3aa50dae956b9f2
Milo <iitalics@gmail.com>
2019-12-24 10:59:48 -0500

    [rkt] bracketing helpers

--- racket/ax-lib/utils.rkt
+++ racket/ax-lib/utils.rkt
@@ -3,10 +3,64 @@
  ; (color->rgb c) -> (list byte byte byte)
  ; c : (or "red" "green" "blue")
  color->rgb
+
+ ; (call/bracket rsrc destroy f) -> A
+ ; rsrc    : R
+ ; destroy : [R -> void]
+ ; f       : [R -> A]
+ call/bracket
+
+ ; (with-bracket [id expr destroy-fn]
+ ;   body ...+)
+ with-bracket
  )
+
+(module+ test
+  (require
+   rackunit
+   racket/port))
 
 (define (color->rgb c)
   (case c
     [("red")   '(255 0 0)]
     [("green") '(0 255 0)]
     [("blue")  '(0 0 255)]))
+
+(define (call/bracket rsrc destroy f)
+  (define-values [err result]
+    (with-handlers ([exn? (λ (e) (values e #f))])
+      (values #f (f rsrc))))
+  (destroy rsrc)
+  (if err (raise err) result))
+
+(define-syntax-rule (with-bracket [x create destroy-fn]
+                      body ...)
+  (call/bracket create
+                destroy-fn
+                (λ (x) body ...)))
+
+(module+ test
+
+  (define (destroy x) (printf "destroy ~a\n" x))
+
+  (check-equal? (with-output-to-string
+                  (λ ()
+                    (with-bracket [x "thing" destroy]
+                      (printf "hello ~a\n" x))))
+                "hello thing\ndestroy thing\n")
+
+  (check-equal? (with-output-to-string
+                  (λ ()
+                    (with-handlers ([exn? void])
+                      (with-bracket [x "thing" destroy]
+                        (error 't "!!!!")))))
+                "destroy thing\n")
+
+  (check-exn #px"!!!!"
+             (λ ()
+               (with-output-to-string
+                 (λ ()
+                   (with-bracket [x "thing" destroy]
+                     (error 't "!!!!"))))))
+
+  )
