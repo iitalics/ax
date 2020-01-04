a0121d6e6eb08748ca50a0b6d86db60b35c2f160
Milo <iitalics@gmail.com>
2020-01-03 20:23:29 -0500

    [test] test sending events from fortest backend

--- backend/fortest.h
+++ backend/fortest.h
@@ -6,8 +6,14 @@ struct ax_backend {
     struct ax_draw* ds;
     size_t ds_len;
 
+    pthread_mutex_t sig_mx;
+    struct {
+        bool close;
+    } sig;
+
     pthread_cond_t sync;
     pthread_mutex_t sync_mx;
 };
 
 void ax_test_backend_sync_until(struct ax_backend* bac, size_t desired_len);
+void ax_test_backend_sig_close(struct ax_backend* bac);
